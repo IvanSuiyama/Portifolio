198fdb9baff619b6555ff7ee4709c61894c2ec37
IvanSuiyama <ivan.suiya@gmail.com>
2022-11-03 12:02:21 -0300

    implementando o flask/font externa

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def homepage():
+    return render_template('Index.html')
+
+@app.route('/Projeto')
+def projeto():
+    return render_template('Projeto.html')
+
+@app.route('/Estudo')
+def estudo():
+    return render_template('Estudos.html')  
+
+@app.route('/Sobre')
+def sobre():
+    return render_template('Sobre.html')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+    
+    
