25d404a38445256a77b2326a4d5601e7d63d26e8
Ivan Suiyama Silva <111767391+IvanSuiyama@users.noreply.github.com>
2022-12-10 22:44:32 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 def homepage():
     return render_template('Index.html')
 
-@app.route('/https://ivan-portifolio2-0.onrender.com/Projeto')
+@app.route('/Projeto')
 def projeto():
     return render_template('Projeto.html')
 
